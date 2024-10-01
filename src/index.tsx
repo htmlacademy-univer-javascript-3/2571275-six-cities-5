719553dd30e5562b04214e5dcc3b0b50c7249f2c
ITSigma <ascoolasanton@gmail.com>
2024-10-01 18:37:14 +0500

    subtask 1

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import MainScreen from './pages/main-screen/main-screen';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -8,5 +9,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <h1>Hello, World!</h1>
+    <MainScreen/>
   </React.StrictMode>
 );
