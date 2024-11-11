af62fb967a22637a5387796225462c860d5d15a4
ITSigma <ascoolasanton@gmail.com>
2024-11-11 19:59:55 +0500

    delete bookmark

--- src/pages/main-screen/components/offer-card.tsx
+++ src/pages/main-screen/components/offer-card.tsx
@@ -1,11 +1,10 @@
-﻿import OfferCardBookmark from './offer-card-bookmark.tsx';
-import Offer from '../../../models/offer.ts';
+﻿import Offer from '../../../models/offer.ts';
 
 type OfferCardProps = {
   offer: Offer;
   onMouseOver: () => void;
   onMouseOut: () => void;
-}
+};
 
 function OfferCard({offer, onMouseOver, onMouseOut}: OfferCardProps): JSX.Element {
   const {isFavorite, isPremium, previewImage, type, title, price, rating} = offer;
@@ -31,7 +30,15 @@ function OfferCard({offer, onMouseOver, onMouseOut}: OfferCardProps): JSX.Elemen
             <b className="place-card__price-value">&euro;{price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <OfferCardBookmark isFavorite={isFavorite} />
+          <button
+            className={`place-card__bookmark-button ${isFavorite && 'place-card__bookmark-button--active'} button`}
+            type="button"
+          >
+            <svg className="place-card__bookmark-icon" width="18" height="19">
+              <use xlinkHref="#icon-bookmark"></use>
+            </svg>
+            <span className="visually-hidden">{isFavorite ? 'In bookmarks' : 'To bookmarks'}</span>
+          </button>
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
