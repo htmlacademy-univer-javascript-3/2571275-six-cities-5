75e99cebcb4ad23c00b745e71b36007e82128b17
ITSigma <ascoolasanton@gmail.com>
2024-12-09 01:51:37 +0500

    --

--- src/mocks/favourites-mock.ts
+++
@@ -1,6 +0,0 @@
-﻿import Offer from '../models/offer.ts';
-import offersMock from './offers-mock.ts';
-
-const favouritesMock : Offer[] = offersMock.filter((offer) => offer.isFavorite);
-
-export default favouritesMock;
